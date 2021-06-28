13c3c296d271c01566737ebf3e1e525cd5fc7476
N8Brown <nathan.a.brown@outlook.com>
2021-06-28 19:32:02 -0400

    initial commit

--- weather_app/weather/views.py
+++ weather_app/weather/views.py
@@ -1,19 +1,64 @@
 from django.shortcuts import render
 
-# https://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode=04090&distance=5&API_KEY=366D1BA7-2827-4B6B-B434-4C432C57F9B8
+# api_request = requests.get('http://api.weatherapi.com/v1/forecast.json?key=4f506553666f42c0b5f190311211706&q=04090&days=3&aqi=no&alerts=yes')
 
-# Create your views here.
-def home(request):
-    import json
-    import requests
 
-    api_request = requests.get("https://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode=04090&distance=5&API_KEY=366D1BA7-2827-4B6B-B434-4C432C57F9B8")
+def weather(request):
+    import json, requests, datetime
+    from .api_key import key
 
-    try:
-        api_response = json.loads(api_request.content)
-    except Exception as e:
-        api_response = "Error fetching data."
+    if request.method == 'POST':
+        if len(request.POST['location']) > 0:
+            location = request.POST['location']
 
-    return render(request, 'home.html', {
-        'api_response': api_response
-    })
+            api_request = requests.get('http://api.weatherapi.com/v1/forecast.json?key=' + key + '&q=' + location + '&days=3&aqi=no&alerts=yes')
+        
+            try:
+                api_response = json.loads(api_request.content)
+            except:
+                api_response = 'Error'
+
+            if api_response == 'Error':
+                pass
+            else:
+                if 'error' in api_response:
+                    if api_response['error']['code'] == 1006:
+                        alert = f"You searched: {location}. {api_response['error']['message']}"
+                    else:
+                        alert = "Something went wrong. Please try again later."
+                    return render(request, 'weather.html', {'alert':alert})
+                else:
+                    days_str = [day['date']+' ' for day in api_response['forecast']['forecastday']]
+                    sunrises_str = [day['astro']['sunrise'] for day in api_response['forecast']['forecastday']]
+                    sunsets_str = [day['astro']['sunset'] for day in api_response['forecast']['forecastday']]
+
+                    sunrises_day_time = list(map(str.__add__, days_str, sunrises_str))
+                    sunsets_day_time = list(map(str.__add__, days_str, sunsets_str))
+
+                    sunrises = [datetime.datetime.strptime(date, '%Y-%m-%d %I:%M %p') for date in sunrises_day_time]
+                    sunsets = [datetime.datetime.strptime(date, '%Y-%m-%d %I:%M %p') for date in sunsets_day_time]
+
+                    day_segments = [datetime.datetime.strptime(day['date'], '%Y-%m-%d').ctime().split(' ') for day in api_response['forecast']['forecastday']]
+
+                    day1 = 'Today'
+                    day2 = day_segments[1][0] + ' ' + day_segments[1][2]
+                    day3 = day_segments[2][0] + ' ' + day_segments[2][2]
+                    
+                    current_time = datetime.datetime.strptime(api_response['location']['localtime'], '%Y-%m-%d %H:%M')
+
+                    context = {
+                        'api_response':api_response,
+                        'current_time':current_time,
+                        'day1':day1,
+                        'day2':day2,
+                        'day3':day3,
+                        'sunrises':sunrises,
+                        'sunsets':sunsets,
+                    }
+
+                    return render(request, 'weather.html', context)
+        else:
+            alert = "Please enter a location."
+            return render(request, 'weather.html', {'alert':alert})
+    else:
+        return render(request, 'weather.html')
